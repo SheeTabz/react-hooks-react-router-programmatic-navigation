7b91d8ec417eb6fc8ca446496ceb1b14a3d419c6
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-07 08:55:19 +0300

    Done

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,7 @@ function Login({ setIsLoggedIn }) {
     setIsLoggedIn(true);
 
     // after logging the user in, redirect to the home page!
-    history.push("/home");
+    history.push("/");
   }
 
   return (
